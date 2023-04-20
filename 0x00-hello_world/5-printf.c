00d3a2fb2dbc0ee1940a4527229768b9c931d66e
Etta-Diego <onyinyechiikediego@gmail.com>
2023-04-20 16:53:26 -0700

    Tasks zero to five

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -  A c program to display a line of thought, followed by a new line
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art/n")
+		return (0);
+}
