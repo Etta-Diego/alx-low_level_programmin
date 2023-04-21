890ea18136781bab1e2603758ddbfa4a44c3a921
Etta-Diego <onyinyechiikediego@gmail.com>
2023-04-20 17:14:28 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,9 @@
  * main -  A c program to display a line of thought, followed by a new line
  * Return: 0 (success)
  */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art/n")
-		return (0);
+	printf("with proper grammar, but the outcome is a piece of art.\n")
+	return (0);
 }
