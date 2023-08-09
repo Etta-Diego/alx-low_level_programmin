437cb5fe6b4ba47b4d96ef8feb1e27e577c3794d
Etta-Diego <onyinyechiikediego@gmail.com>
2023-08-08 20:39:42 -0700

    Change in Task 1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,8 +20,12 @@ char *create_array(unsigned int size, char c)
 	if (a == NULL)
 		return (NULL);
 
-	for (i = 0; i < size; i++)
+	 i = 0;
+	while (i < size)
+	{
 		a[i] = c;
+	i++;
+}
 
 	return (a);
 }
