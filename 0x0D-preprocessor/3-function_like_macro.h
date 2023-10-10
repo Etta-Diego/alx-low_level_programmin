8fc5760357a9b668f76e2784e3494a9da299ebc2
Etta-Diego <onyinyechiikediego@gmail.com>
2023-10-10 11:21:38 -0700

    fixed task 0 and 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,12 @@
 #ifndef ABS_H
 #define ABS_H
 
+/**
+ * abs - function-like macro that computes the absolute value of a number x
+ * @x: value of a number to be computed
+ *
+ */
+
 #define ABS(x) ((x < 0) ? (x * (-1)) : (x > 0) ? (x * 1) : 0)
 
 #endif
