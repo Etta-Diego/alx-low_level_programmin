c88886ce227190dfc7b9da0dad51021a7602da6f
Etta-Diego <onyinyechiikediego.gmail.com>
2023-12-08 19:47:41 +0100

    task 6 update

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -3,6 +3,7 @@
  * sum_dlistint -  sums all the data (n) of a dlistint_t linked list.
  * @head: pointer to the first node
  *
+ * Return: sum of all th data
  */
 
 int sum_dlistint(dlistint_t *head)
