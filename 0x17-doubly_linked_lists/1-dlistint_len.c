058ba39a43d528425c10f41285fa58eef6b2230f
Etta-Diego <onyinyechiikediego.gmail.com>
2023-12-08 03:40:18 +0100

    task 1 correction

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,6 +1,6 @@
-#include "lists.h"
+iinclude "lists.h"
 /**
- * dlistint_len - function that returns the number of elements 
+ * dlistint_len - function that returns the number of elements
  * @h: the head parameter
  *
  * Return: number of elements
@@ -8,11 +8,12 @@
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count;
+
 	count = 0;
 	while (h != NULL)
 	{
 		h = h->next;
 		count++;
 	}
-	return count;
+	return (count);
 }
