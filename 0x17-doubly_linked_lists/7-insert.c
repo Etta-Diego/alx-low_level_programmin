95595d23c52fd20bfc58da833f2ea2ff9fca62d3
Etta-Diego <onyinyechiikediego.gmail.com>
2023-12-09 03:03:07 +0100

    task 7 update correction 2

--- 0x17-doubly_linked_lists/7-insert.c
+++ 0x17-doubly_linked_lists/7-insert.c
@@ -7,50 +7,55 @@
  *
  * Return: the address of the new node, or NULL if it failed
  */
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	struct dlistint_s *new_node, *current_node;
+	struct dlistint_s *temp, *new_node, *current_node;
 	size_t count;
 
 	if (*h == NULL)
 	{
-		return (NULL);
+		return NULL;
 	}
 	new_node = malloc(sizeof(struct dlistint_s));
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
-
+	temp = (*h);
+	count = 0;
+	new_node->next = NULL;
 	new_node->n = n;
-	if (idx == 0)
+	new_node->prev = NULL;
+
+	if (idx == 0 && *h != NULL)
 	{
-		new_node->next = (*h);
+		new_node->next =(*h);
 		new_node->prev = NULL;
-		if (*h != NULL)
-		{
-			(*h)->prev = new_node;
-		}
+		(*h)->prev = new_node;
 		(*h) = new_node;
-		return (new_node);
 	}
-	current_node = (*h);
-	for (count = 0; current_node != NULL && count < idx - 1; count++)
+	else
 	{
-		current_node = current_node->next;
+	while (temp->next != NULL)
+	{
+		if (count == idx)
+		{
+			current_node = temp->next;
+			temp->next = new_node;
+			current_node->prev = new_node;
+			new_node->next = current_node;
+			new_node->prev = temp;
+			return (new_node);
+		}
+		temp = temp->next;
+		count++;
 	}
-	if (current_node == NULL)
+	}
+	if (temp->next == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
-	new_node->next = current_node->next;
-	new_node->prev = current_node;
-	if (current_node->next != NULL)
-	{
-		current_node->next->prev = new_node;
-	}
-	current_node->next = new_node;
 	return (new_node);
 }
